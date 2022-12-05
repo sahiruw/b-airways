835da2440dbd826eae6b2fb9a72034fd3cd69d25
sahiru <sahiruwrcg@gmail.com>
2022-12-05 14:24:56 +0530

    DEV2

--- server/controllers/app.js
+++ server/controllers/app.js
@@ -5,12 +5,12 @@ const register = require("./register")
 const login = require("./login")
 const isLogged = require("./islogged")
 const logout = require("./logout")
-const getUser = require("./getUser")
+// const getUser = require("./getUser")
 
 router.post("/register", register)
 router.post("/login", login)
 router.get("/islogged", isLogged)
-router.get("/getUser", getUser)
+// router.get("/getUser", getUser)
 router.post("/logout", logout)
 
 module.exports = router
