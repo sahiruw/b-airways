25ce5b6bab2e4dd95060ed9f19eb05285857a41f
Amila29 <amilakasun2000@gmail.com>
2022-12-20 17:13:47 +0530

    flightsb

--- client/src/components/flightSearchBar.js
+++ client/src/components/flightSearchBar.js
@@ -9,8 +9,16 @@ const SearchBar = () => {
 
         <div>
 
-            
+            <div className="shadow-lg p-3 mb-5 bg-white rounded" style={style_search_bar}>
+            <input type="text" placeholder="From--"></input>
+            <input type = "text" placeholder="To--" style={{marginLeft : 20}}></input>
 
+            <br></br>
+            <br></br>
+
+            <input type="text" placeholder = "departure date"></input>
+            <button className="btn btn-outline-secondary mx-4" style={{width : 200}}>Search</button>
+            </div>
         </div>
     )
 }
