6666000a210ed381ca9ac97b8f485b3795b0fbac
sahiru <sahiruwrcg@gmail.com>
2022-12-30 01:29:51 +0530

    some work in seatselection

--- client/src/components/booking/seatSelection.js
+++ client/src/components/booking/seatSelection.js
@@ -1,30 +1,100 @@
 import React, { useEffect, useState } from "react";
 
-function SeatSelection(props) {
-  const [aircraftDetails, setairecraftDetails] = useState(null);
+const Seat = ({ id, reserved, onClick }) => {
+  const [isChecked, setIsChecked] = useState(reserved);
 
-  useEffect(() => {
-  fetch("/api/getAircraftDetailsByID?aircraftID=" + props.aircraftID)
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status) {
-          setairecraftDetails(data.data);
-        } else {
-          setairecraftDetails(null);
-        }
-      });
-  }, []);
+  const handleClick = () => {
+    setIsChecked(!isChecked);
+    onClick(id, !isChecked);
+  };
 
   return (
-    <div>
-      <button
-        type="submit"
-        className="btn btn-primary"
-      >
-        Fuck me
-      </button>
-      <h1>{props.aircraftID}</h1>
-    </div>
+    <td>
+      <div className="seat">
+        <input
+          type="checkbox"
+          id={id}
+          checked={isChecked}
+          onChange={handleClick}
+        />
+        <label htmlFor={id}>{id}</label>
+      </div>
+    </td>
+  );
+};
+
+const Seats = ({ seats, onSeatClick }) => {
+  return (
+    <table className="seats">
+      {seats.map((row) => {
+        return (
+          <tr>
+            {row.map((seat) => (
+              <Seat
+                id={seat.id}
+                reserved={seat.reserved}
+                onClick={onSeatClick}
+              />
+            ))}
+          </tr>
+        );
+      })}
+    </table>
+  );
+};
+
+function SeatSelection(props) {
+  const [aircraftDetails, setairecraftDetails] = useState();
+  let seatClass = props.seatClass || "Economy";
+
+  const [seats, setSeats] = useState([
+    [
+      { id: "A1", reserved: false },
+      { id: "A2", reserved: false },
+      { id: "A3", reserved: false },
+    ],
+    [
+      { id: "B1", reserved: false },
+      { id: "B2", reserved: false },
+      { id: "B3", reserved: false },
+    ],
+  ]);
+
+  const [bookedSeatIds, setBookedSeatIds] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/getAircraftDetailsByID?aircraftID=" + props.aircraftID)
+      .then((res) => res.json())
+      .then((data) => {
+        setairecraftDetails(data.data);
+      });
+  },[]);
+
+  const handleSeatClick = (id, reserved) => {
+    setSeats((prevSeats) =>
+      prevSeats.map((row) =>
+        row.map((seat) => (seat.id === id ? { ...seat, reserved } : seat))
+      )
+    );
+  };
+
+  let platinumSeats = aircraftDetails ? aircraftDetails.Platinum_seats : 0;
+  let businessSeats = aircraftDetails ? aircraftDetails.Business_seats : 0;
+  let economySeats = aircraftDetails ? aircraftDetails.Economy_seats : 0;
+
+  //the order for ids is: Platinum, Business, Economy
+  let idsStart = 0;
+  if (seatClass == "Business") {idsStart += platinumSeats}
+  else if (seatClass == "Economy") {idsStart += platinumSeats + businessSeats}
+
+  return (
+    <>
+      <h1>Seat Selection</h1>
+      {seats.map((row) =>
+        row.map((seat) => " | id: " + seat.id + " | reserved: " + seat.reserved)
+      )}
+      <Seats seats={seats} onSeatClick={handleSeatClick} />
+    </>
   );
 }
 
