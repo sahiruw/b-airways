d902683fea7390680b03016278ae7c769888e1c1
sahiru <sahiruwrcg@gmail.com>
2022-12-29 17:25:51 +0530

    pw

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,5 +1,5 @@
 DELIMITER $$
-CREATE FUNCTION insert_member(p_firstname VARCHAR(10), p_lastname VARCHAR(10), p_email VARCHAR(30), p_password VARCHAR(50), p_mem_cat_id INT, p_dob DATE, p_country VARCHAR(15), p_tele_no VARCHAR(12))
+CREATE FUNCTION insert_member(p_firstname VARCHAR(10), p_lastname VARCHAR(10), p_email VARCHAR(30), p_password VARCHAR(150), p_mem_cat_id INT, p_dob DATE, p_country VARCHAR(15), p_tele_no VARCHAR(12))
 RETURNS INT
 DETERMINISTIC
 BEGIN
