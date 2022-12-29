0421b26967c66e3ecdcbab57e820452247b68e76
sahiru <sahiruwrcg@gmail.com>
2022-12-29 16:46:02 +0530

    functions sql

---
+++ sql/functions.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE FUNCTION insert_member(p_firstname VARCHAR(10), p_lastname VARCHAR(10), p_email VARCHAR(30), p_password VARCHAR(50), p_mem_cat_id INT, p_dob DATE, p_country VARCHAR(15), p_tele_no VARCHAR(12))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+  INSERT INTO customer (type) VALUES ("member");
+  INSERT INTO member (ID, firstname, lastname, email, password, mem_cat_id, dob, country, tele_no)
+  VALUES (LAST_INSERT_ID(), p_firstname, p_lastname, p_email, p_password, p_mem_cat_id, p_dob, p_country, p_tele_no);
+  RETURN LAST_INSERT_ID();
+END$$
+DELIMITER ;
