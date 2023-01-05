4b619e25a0cd33146a2fcbdbcf1e50c1732ce973
Amila29 <amilakasun2000@gmail.com>
2023-01-05 14:37:36 +0530

    commited_sql

--- sql/views.sql
+++ sql/views.sql
@@ -2,6 +2,28 @@ CREATE
     ALGORITHM = UNDEFINED 
     DEFINER = `root`@`localhost` 
     SQL SECURITY DEFINER
+VIEW `flights_and_aircrafts` AS
+    SELECT 
+        `f`.`ID` AS `ID`,
+        `at`.`platinum_seats` AS `platinum_seats`,
+        `at`.`Bussiness_seats` AS `Bussiness_seats`,
+        `at`.`Economy_seats` AS `Economy_seats`,
+        `at`.`name` AS `aircraft_name`,
+        `f`.`path_iD` AS `path_ID`,
+        `f`.`status` AS `status`,
+        `f`.`departure_time` AS `departure_time`,
+        `f`.`arrival_time` AS `arrival_time`
+    FROM
+        ((`flight` `f`
+        JOIN `aircraft` `a` ON ((`f`.`aircraft_ID` = `a`.`ID`)))
+        JOIN `aircraft_type` `at` ON ((`a`.`type_ID` = `at`.`ID`)))
+
+
+
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
 VIEW `flight_details4` AS
     SELECT 
         `f`.`ID` AS `ID`,
@@ -13,28 +35,11 @@ VIEW `flight_details4` AS
         `at`.`Bussiness_seats` AS `Bussiness_seats`,
         `at`.`Economy_seats` AS `Economy_seats`,
         `b`.`seat_type` AS `seat_type`,
-        `at`.`name` AS `aircraft_name`
+        `at`.`name` AS `aircraft_name`,
+        `f`.`status` AS `flight_status`
     FROM
         ((((`flight` `f`
         JOIN `paths` `p` ON ((`f`.`path_iD` = `p`.`ID`)))
         JOIN `aircraft` `a` ON ((`f`.`aircraft_ID` = `a`.`ID`)))
         JOIN `aircraft_type` `at` ON ((`a`.`type_ID` = `at`.`ID`)))
         LEFT JOIN `booking` `b` ON ((`f`.`ID` = `b`.`flight_ID`)))
-    
-
-
-CREATE 
-    ALGORITHM = UNDEFINED 
-    DEFINER = `root`@`localhost` 
-    SQL SECURITY DEFINER
-VIEW `flights_and_aircrafts` AS
-    SELECT 
-        `f`.`ID` AS `ID`,
-        `at`.`platinum_seats` AS `platinum_seats`,
-        `at`.`Bussiness_seats` AS `Bussiness_seats`,
-        `at`.`Economy_seats` AS `Economy_seats`,
-        `at`.`name` AS `name`
-    FROM
-        ((`flight` `f`
-        JOIN `aircraft` `a` ON ((`f`.`aircraft_ID` = `a`.`ID`)))
-        JOIN `aircraft_type` `at` ON ((`a`.`type_ID` = `at`.`ID`)))
