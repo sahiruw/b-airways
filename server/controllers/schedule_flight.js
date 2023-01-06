173226f5f0eb3c9788df5986c2979757a2bf9802
Amila29 <amilakasun2000@gmail.com>
2023-01-06 12:33:38 +0530

    commited - Amila

--- server/controllers/schedule_flight.js
+++ server/controllers/schedule_flight.js
@@ -2,11 +2,17 @@ const db = require("../routes/db-config");
 const admin = require("../models/adminModel");
 
 const Schedule_Flight = async (req,res) => {
-    const[start_dest,end_dest,aircraft_ID,departure_time,arrival_time,cost] = req.body;
+    const{start_destination,
+        end_destination,
+        aircraft_id,
+        departure_date_time,
+        arrival_date_time,
+        cost} = req.body;
+    console.log(start_destination,end_destination,aircraft_id,departure_date_time,arrival_date_time,cost);
 
-    await admin.ScheduleFlight(aircraft_ID,start_dest,end_dest,departure_time,arrival_time,cost);
+    let data = await admin.ScheduleFlight(aircraft_id,start_destination,end_destination,departure_date_time,arrival_date_time,cost);
 
-    console.log("OKKK")
+    
 
     return res.json({stats:1,message:"Flight has been scheduled successfully"});
 
